42b2146103b5a7378da2647411a9e8c0f0259f97
Sean Stephens <seanstephens29@gmail.com>
2018-02-09 18:56:43 -0700

    fixed production builds

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -16,17 +16,21 @@ function getBuildInfo(version) {
 	return { version: v, build: b };
 }
 
+function getRevision() {
+	return this.cmd(`git log -1 --format=%H`, { hidecmd: true }).then(hash => hash.substr(0, 7));
+}
+
 module.exports = createCommand({
 	name: 'deploy',
 	description: 'deploy build to server.',
 	alias: 'd',
-	args: ['<branch>'],
+	args: ['[branch]', '[tag]'],
 	
 	options: [
 		//{ cmd: '--prod', short: '-p', desc: 'production tags can only be deleted with --prod option applied.' }
 	],
 	
-	run(branch) {
+	run(branch, tag) {
 		let cwd = process.cwd();
 		let pkgInfo = require(path.join(cwd + '/package.json'));
 		let version = pkgInfo.version;
@@ -35,27 +39,42 @@ module.exports = createCommand({
 		baseVer = baseVer.split('.');
 		baseVer = [ baseVer[0], baseVer[1] ].join('.');
 
-		let [ build, ] = buildVer.split('.');
-		if (branch === 'master') {
-			build = 'canary';
-		} else if (branch !== baseVer || isEmpty(buildVer)) {
-			return this.resolve("Not a release branch");
-		}
-
-		return release.call(this, build, true, true, false, branch).then(res => {
-			let info = getBuildInfo(res);
-			return prune.call(this, info.version, info.build, 5, true).then(result => {
-				this.ui.info(result);
-				return this.cmd(`git log -1 --pretty=oneline`, { hidecmd: true }).then(log => {
-					let [ hash ] = log.split(' ');
-					let revision = hash.substr(0, 7);
-					return this.cmd(`ember deploy ${info.build}`).then(() => {
-						return this.cmd(`ember deploy:activate --revision ${revision} ${info.build}`).then(() => {
+		if (!isEmpty(tag)) {
+			if (/v[0-9]+\.[0-9]+\.[0-9]+$/.test(tag)) {
+				this.ui.info('Preparing production deploy...');
+				return getRevision.call(this).then(revision => {
+					return this.cmd(`ember deploy production`).then(() => {
+						return this.cmd(`ember deploy:activate --revision ${revision} production`).then(() => {
 							return this.resolve('Deploy finished!');
 						});
 					});
 				});
+			} else {
+				this.resolve('Not a production tag. Skipping deploy');
+			}
+		} else if (!isEmpty(branch)) {
+			let [ build, ] = buildVer.split('.');
+			if (branch === 'master') {
+				build = 'canary';
+			} else if (branch !== baseVer || isEmpty(buildVer)) {
+				return this.resolve("Not a release branch");
+			}
+
+			return release.call(this, build, true, true, false, branch).then(res => {
+				let info = getBuildInfo(res);
+				return prune.call(this, info.version, info.build, 5, true).then(result => {
+					this.ui.info(result);
+					return getRevision.call(this).then(revision => {
+						return this.cmd(`ember deploy ${info.build}`).then(() => {
+							return this.cmd(`ember deploy:activate --revision ${revision} ${info.build}`).then(() => {
+								return this.resolve('Deploy finished!');
+							});
+						});
+					});
+				});
 			});
-		});
+		} else {
+			return this.reject('You must provide a tag or a branch');
+		}
 	}
 });
